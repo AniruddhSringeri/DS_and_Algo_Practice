f760c5d737e4f2595687fc095b121a2ae6e00cb7
Aniruddh <aniruddhsringeri@gmail.com>
2019-10-02 17:39:15 +0530

    Idk

---
+++ Linked_Lists/2.linked_list.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node 
+{
+    int data;
+    struct node* next;
+};
+
+struct node* getnode(int data, struct node* nextNode)
+{
+    struct node* Node = (struct node*)malloc(sizeof(struct node));
+    Node->data = data;
+    Node->next = nextNode;
+    return Node;
+}
+
+struct node* constructList()
+{
+    struct node* head = getnode(1, getnode(2, getnode(3, NULL)));
+    return head;
+}
+
+void printList(struct node* head)
+{
+    struct node* ptr = head;
+    printf("The linked list is:  ");
+    while(ptr)
+    {
+        printf("%d, ", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("\b\b \n");
+}
+
+int main()
+{
+    struct node* head = constructList();
+    printList(head);
+    return 0;
+}
