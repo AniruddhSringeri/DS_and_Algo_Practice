382000547debb9671f13530651eb9d188a2b5182
Aniruddh <aniruddhsringeri@gmail.com>
2019-10-02 18:57:55 +0530

    NULL

---
+++ Linked_Lists/3.linked_list_taking_user_input.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node* next;
+};
+
+struct node* getList(int n)
+{
+    int data;
+    printf("Enter element:  ");
+    scanf("%d", &data);
+    struct node* Node = (struct node*)malloc(sizeof(struct node));
+    Node->data = data;
+    if (n==1)
+    {
+        Node->next = NULL;
+    }
+    else 
+    {
+        Node->next = getList(n-1);
+    }
+    return Node;
+}
+
+void printList(struct node* head)
+{
+    struct node* ptr = head;
+    printf("The linked list is: ");
+    while(ptr)
+    {
+        printf("%d, ", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("\b\b \n");
+}
+
+int main()
+{
+    int n, ch;
+    printf("Program to implement a singly linked list.\n");
+    do{
+        printf("Enter 1 to proceed and anything else to exit:  ");
+        scanf("%d", &ch);
+        switch(ch)
+        {
+            case 1: {
+                do{
+                    printf("Enter the no. of elements to be in the linked list(>=1):  ");
+                    scanf("%d", &n);
+                } while(n<1);
+                struct node* head = getList(n);
+                printList(head);
+                break;
+                }
+                
+            default: exit(0);
+            
+
+        }
+    } while(1);
+    return 0;
+
+}
